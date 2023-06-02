7e0f412a77c6359fff0e465244c21ea9cbe462f4
Jakub Záruba <eflyax42@gmail.com>
2023-06-02 17:28:39 +0200

    [IMP] Load AML SDK

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,10 @@
 <script setup lang="ts">
-import {Game} from './game/Game';
 
-const game = new Game();
-
-const onChangeScarf = () => {
-	const boneNameToTexture = {
-		'pelvis': 'girl/sprites/head.png'
-	};
-	// 'pelvis': 'hand/dragonbones-export/MechArm_ARM.png'
-
-	game.changeTexture(boneNameToTexture);
-};
 </script>
 
 <template>
 	<div>
-		<button @click="onChangeScarf({
-			'scarf.png': 'girl/sprites/scarf.png'
-			})">
-			Change 1
-		</button>
-		<button @click="onChangeScarf({
-			'scarf.png': 'girl/sprites/scarf2.png'
-			})">
-			Change 2
-		</button>
+
 	</div>
 </template>
 
